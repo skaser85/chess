5e950b7f2ef326e4cbf643feffbbf3f65e21489d
Steve Kaser <skaser85@gmail.com>
2025-11-09 13:52:57 -0500

    added a rook

--- main.c
+++ main.c
@@ -1,11 +1,15 @@
 #include <stddef.h>
 #include <raylib.h>
+#include <raymath.h>
 
 #define NOB_IMPLEMENTATION
 #define NOB_STRIP_PREFIX
 #include "./nob.h"
 
+#define SW 1200
+#define SH 720
 #define BLACK_SQR_COLOR 0x1c1916ff
+#define CRAPPY_DIM 100
 
 Rectangle DrawBoard() {
     size_t dim = GetScreenHeight() * 0.9;
@@ -16,11 +20,11 @@ Rectangle DrawBoard() {
     return r;
 }
 
-void DrawCells(Rectangle board) {
+size_t DrawCells(Rectangle board) {
     Color black = GetColor(BLACK_SQR_COLOR);
     size_t cell_dim = board.width / 8;
-    for (size_t y = board.y; y < board.y + board.height - cell_dim; y += cell_dim) {
-        for (size_t x = board.x; x < board.x + board.width - cell_dim; x += cell_dim) {
+    for (size_t y = board.y; y < board.y + board.height ; y += cell_dim) {
+        for (size_t x = board.x; x < board.x + board.width ; x += cell_dim) {
             Rectangle r = { .x = x, .y = y, .width = cell_dim, .height = cell_dim };
             DrawRectangleLinesEx(r, 3, black); 
             if ((y % 2 == 0 && x % 2 == 1) ||
@@ -29,11 +33,12 @@ void DrawCells(Rectangle board) {
             }
         }
     }
+    return cell_dim;
 }
 
 int main(void)
 {
-    InitWindow(1920, 1080, "Chess");
+    InitWindow(SW, SH, "Chess");
 
     const char* crappy_png = "./assets/crappy/crappy.png";
     Texture2D crappy = LoadTexture(crappy_png);
@@ -44,7 +49,11 @@ int main(void)
         ClearBackground(GetColor(0xb77e38FF));
 
         Rectangle board = DrawBoard();
-        DrawCells(board); 
+        size_t cell_dim = DrawCells(board); 
+
+        Rectangle src = { .x = 0, .y = 0, .width = CRAPPY_DIM, .height = CRAPPY_DIM };
+        Rectangle dest = { .x = board.x, .y = board.y, .width = cell_dim*0.9, .height = cell_dim*0.9 };
+        DrawTexturePro(crappy, src, dest, Vector2Zero(), 0, WHITE);
 
         EndDrawing();
     }
